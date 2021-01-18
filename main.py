102eb9103b266e670c6fcf0088c1b4b49cf8fd16
Eric Fabroa <eric.fabroa@ycdsbk12.ca>
2021-01-18 09:07:07 -0500

    Update main.py

--- main.py
+++ main.py
@@ -13,16 +13,25 @@ RED      = ( 255,   0,   0)
 pygame.init()
   
 # Set the width and height of the screen [width, height]
-size = (700, 500)
+size = (750, 580)
 screen = pygame.display.set_mode(size)
+
+# Load and set up graphics.
+background_image = pygame.image.load("saturn_family1.jpg").convert()
+player_image = pygame.image.load("player.png").convert()
+player_image.set_colorkey(BLACK)
+
+# Set positions of graphics
+background_position = [0, 0]
  
-pygame.display.set_caption("My Game")
+pygame.display.set_caption("Images Demo")
  
 #Loop until the user clicks the close button.
 done = False
  
 # Used to manage how fast the screen updates
 clock = pygame.time.Clock()
+
  
 # -------- Main Program Loop -----------
 while not done:
@@ -32,12 +41,21 @@ while not done:
             done = True # Flag that we are done so we exit this loop
   
     # --- Game logic should go here
+
+    # Get the current mouse position. This returns the position
+    # as a list of two numbers.
+    player_position = pygame.mouse.get_pos()
+    player_x = player_position[0]
+    player_y = player_position[1]
  
     # --- Drawing code should go here
+
+    # Copy image to screen:
+    screen.blit(background_image, background_position)
+
+    # Copy image to screen:
+    screen.blit(player_image, [player_x, player_y])
      
-    # First, clear the screen to white or whatever background colour. 
-    # Don't put other drawing commands above this, or they will be erased with this command.
-    screen.fill(WHITE)
      
     # --- Go ahead and update the screen with what we've drawn.
     pygame.display.flip()
